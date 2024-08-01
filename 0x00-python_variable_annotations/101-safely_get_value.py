bdc38c69378a23e3b5b969b847fa2422879aa6ed
Sufi-to <3thomas225@gmail.com>
2024-08-01 15:06:36 +0400

    Typevar stuff

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module for learning about TypeVar when usin annotations in python."""
+
+
+from typing import Mapping, Union, Any, TypeVar
+
+
+T = TypeVar('T')
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]=None) -> Union[Any, T]:
+    """Returns the key value pairs in the dictionary or None."""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
