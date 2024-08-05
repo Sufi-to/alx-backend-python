8ab9b76281e7de743e1799f18120abeda3e726b3
Sufi-to <3thomas225@gmail.com>
2024-08-06 00:22:10 +0400

    create a async task object

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module about a function that creates async tasks."""
+
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    return asyncio.create_task(wait_random())
