c8342419e6f227861949f75afb7d031f637a0ccc
Sufi-to <3thomas225@gmail.com>
2024-08-06 22:13:57 +0400

    gather the asyncs

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -11,10 +11,6 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Return a float of how long the operation took."""
     start_time = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    gather_asyncs = [async_comprehension() for i in range(4)]
+    results = await asyncio.gather(*gather_asyncs)
     return time.time() - start_time
