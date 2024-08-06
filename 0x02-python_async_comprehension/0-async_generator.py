bd6e88e537276de5eed194481ad9f834cad7da24
Sufi-to <3thomas225@gmail.com>
2024-08-06 21:38:43 +0400

    generate floats async

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,11 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
+    """Yields a random float every time a loop is finished."""
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
